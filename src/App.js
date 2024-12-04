8a85b8254f224d6df44d2976d78a369c16f49056
whyshurik <fielaozxc@gmail.com>
2024-12-04 18:57:32 +0200

    added routes integration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Header from "./components/Header/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Catalog from "./components/Catalog";
 
 function App() {
-  return (
-      <>
-          <Header></Header>
-      </>
-  );
+    return (
+        <Router>
+            <Header/>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/catalog" element={<Catalog />}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/Catalog/index.js
@@ -0,0 +1,7 @@
+export default function Catalog () {
+    return <ul>
+        <ol>cat1</ol>
+        <ol>cat2</ol>
+        <ol>cat3</ol>
+    </ul>
+}
